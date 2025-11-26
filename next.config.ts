7e1730c2c0a19327bed0371b0eb832f3c998760c
rodriguesfundo <rodriguesdomingosf@gmail.com>
2025-11-26 10:31:01 +0200

    try script deploy

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,41 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  // basePath: '/app',
-  output: 'export',
-  assetPrefix: './',
+  output: "export",
   trailingSlash: true,
-  basePath: '/css',
+  basePath: "/css",
+
+  // assetPrefix: './', // ⛔ REMOVER ESTA LINHA
+
+  generateBuildId: async () => {
+    return `build-${Date.now()}`;
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=0, must-revalidate",
+          },
+        ],
+      },
+      {
+        source: "/_next/static/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
+
+  poweredByHeader: false,
+  compress: true,
 };
 
 export default nextConfig;
