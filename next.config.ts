785f9373e3a1757af92305d018ef487d277adf9d
rodriguesfundo <rodriguesdomingosf@gmail.com>
2026-04-22 21:15:52 +0200

    fix: update next.config.ts with new configuration options and headers

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,41 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  trailingSlash: true,
+  basePath: "/css",
+
+  // assetPrefix: './', // ⛔ REMOVER ESTA LINHA
+
+  generateBuildId: async () => {
+    return `build-${Date.now()}`;
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=0, must-revalidate",
+          },
+        ],
+      },
+      {
+        source: "/_next/static/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
+
+  poweredByHeader: false,
+  compress: true,
   /* config options here */
   // basePath: '/app',
   // output: 'export',
